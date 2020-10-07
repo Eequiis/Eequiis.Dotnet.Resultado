29631da5c8495df2e34823151a0a60f03c1b91d3
Eequiis Vásquez <42697474+Eequiis@users.noreply.github.com>
2020-10-07 13:19:34 +0200

    Métodos AsExito y AsFracaso hechos virtuales (devuelven null) y documentación mejorada

--- Eequiis.Dotnet.Resultado/Modelo/Resultado.cs
+++ Eequiis.Dotnet.Resultado/Modelo/Resultado.cs
@@ -3,86 +3,96 @@
 namespace Eequiis.Dotnet.Resultado
 {
 	/// <summary>
-	/// <para>Representa el resultado de una operación que no devuelve ningún valor, o cuyo valor de retorno es
-	/// un booleano que solo indica si la operación ha tenido éxito o no.</para>
-	/// <para>En caso de que una operación devuelva un valor booleano que no indique el éxito o fracaso de la operación
-	/// invocada, se recomienda el uso de la clase <see cref="Resultado{T}"/> genérica, instanciándola mediante el uso
-	/// de la clase Resultado&lt;bool&gt;.
+	/// <para>Representa el resultado de una operación que no devuelve ningún valor, y que solo indica si dicha
+	/// operación se completó exitosamente, o si por el contrario fracasó.</para>
+	/// <para>Para representar u resultado de éxito o fracaso, hay que instanciar las clases <see cref="Exito"/> y
+	/// <see cref="Fracaso"/>, que heredan de esta. Sin embargo, del lado cliente, ambos tipos de resultado se pueden
+	/// almacenar en variables de tipo <c>Resultado</c>, que provee métodos para comprobar el éxito o fracaso de la
+	/// operación, lo que permitiría hacer <i>castings</i> con garantía de éxito.</para>
+	/// <para>Las operaciones que devuelven valores booleanos que solo indican el éxito o fracaso de las mismas también
+	/// deberían utilizar esta clase como tipo de retorno. Sin embargo, en caso de que una operación devuelva un valor
+	/// booleano que no indica el éxito o fracaso de la misma, se recomienda el uso de la clase
+	/// <see cref="Resultado{T}"/> genérica, instanciándola como <c>Resultado&lt;bool&gt;</c>.
 	/// </para>
 	/// </summary>
 	public abstract class Resultado : TResultado
 	{
 		/// <summary>
-		/// <para>Obtiene una vista exitosa del resultado.</para>
-		/// <para>Si el resultado no es una instancia de <see cref="Exito"/>, el método devolverá null.</para>
+		/// <para>Obtiene una vista exitosa de este resultado.</para>
+		/// <para>Si este resultado no es una instancia de <see cref="Exito"/>, el método devolverá <c>null</c>.</para>
 		/// </summary>
-		/// <returns>Una vista exitosa del resultado, o null.</returns>
-		public abstract Exito AsExito();
+		/// <returns>Una vista exitosa de este resultado, o <c>null</c>.</returns>
+		public virtual Exito AsExito() => null;
 
 		/// <summary>
-		/// <para>Obtiene una vista fallida del resultado.</para>
-		/// <para>Si el resultado no es una instancia de <see cref="Fracaso"/>, el método devolverá null.</para>
+		/// <para>Obtiene una vista de fracaso de este resultado.</para>
+		/// <para>Si este resultado no es una instancia de <see cref="Fracaso"/>, el método devolverá <c>null</c>.
+		/// </para>
 		/// </summary>
-		/// <returns>Una vista fallida del resultado, o null.</returns>
-		public abstract Fracaso AsFracaso();
+		/// <returns>Una vista de fracaso de este resultado, o <c>null</c>.</returns>
+		public virtual Fracaso AsFracaso() => null;
 
 		/// <summary>
-		/// <para>Obtiene un resultado, instancia de <see cref="Exito"/> o <see cref="Fracaso"/>, en base a un valor
-		/// booleano y un mensaje de error.</para>
-		/// <para>Si el valor booleano es true, se devolverá un resultado exitoso, mientras que si es false,
-		/// se devolverá un resultado fallido, acompañado por el mensaje indicado en el parámetro.</para>
+		/// <para>Obtiene un resultado, instancia de <see cref="Exito"/> o de <see cref="Fracaso"/>, en base a un valor
+		/// o expresión booleanos y un mensaje de error.</para>
+		/// <para>Si el valor booleano es <c>true</c>, se devolverá un resultado exitoso, mientras que si es
+		/// <c>false</c>, se devolverá un resultado de fracaso, acompañado por el mensaje indicado. Y si el mensaje de
+		/// error indicado no es significativo (nulo, vacío o blanco), se sustituirá por un mensaje genérico, como se
+		/// indica en <see cref="Fracaso(string)"/>.</para>
 		/// </summary>
 		/// <param name="exito">Indica si el resultado a obtener es exitoso.</param>
-		/// <param name="error">Mensaje de error en caso de que el resultado a construir sea un <see cref="Fracaso"/>.
-		/// </param>
-		/// <returns>Un resultado exitoso o fallido en función del valor booleano indicado.</returns>
-		public static Resultado Get(bool exito, string error)
+		/// <param name="mensaje">Mensaje asociado en caso de que el resultado a construir sea de fracaso.</param>
+		/// <returns>Un resultado de éxito o fracaso en función del valor booleano indicado.</returns>
+		public static Resultado Get(bool exito, string mensaje)
 		{
 			if (exito) return new Exito();
-			else return new Fracaso(error);
+			else return new Fracaso(mensaje);
 		}
 	}
 
 	/// <summary>
-	/// <para>Representa el resultado de una operación que devuelve un valor de un tipo parametrizado.</para>
-	/// <para>En caso de que la operación devuelva un valor booleano solo indique el éxito o fracaso de la operación,
-	/// o que no tenga valor de retorno (void), se recomienda el uso de la clase <see cref="Resultado"/> no
-	/// parametrizada.</para>
+	/// <para>Representa el resultado de una operación que devuelve un valor del tipo parametrizado indicado.</para>
+	/// <para>En caso de que la operación devuelva un valor booleano que solo indica el éxito o fracaso de la misma, o
+	/// que no devuelva ningún valor (<c>void</c>), se recomienda el uso de la clase <see cref="Resultado"/> no
+	/// genérica.</para>
 	/// </summary>
-	/// <typeparam name="T">Tipo parametrizado del valor devuelto por la operación.</typeparam>
+	/// <typeparam name="T">Tipo del valor devuelto por la operación.</typeparam>
 	public abstract class Resultado<T> : TResultado
 	{
 		/// <summary>
-		/// <para>Obtiene una vista exitosa del resultado que llama al método.</para>
-		/// <para>Si el resultado no es instancia de <see cref="Exito{T}"/>, el método devolverá null.</para>
+		/// <para>Obtiene una vista exitosa de este resultado.</para>
+		/// <para>Si este resultado no es una instancia de <see cref="Exito{T}"/>, el método devolverá <c>null</c>.
+		/// </para>
 		/// </summary>
-		/// <returns>Una vista parametrizada exitosa del resultado, o null.</returns>
-		public abstract Exito<T> AsExito();
+		/// <returns>Una vista exitosa de este resultado, o <c>null</c>.</returns>
+		public virtual Exito<T> AsExito() => null;
 
 		/// <summary>
-		/// <para>Obtiene una vista fallida del resultado que llama al método.</para>
-		/// <para>Si el resultado no es instancia de <see cref="Fracaso{T}"/>, el método devolverá null.</para>
+		/// <para>Obtiene una vista de fracaso de este resultado.</para>
+		/// <para>Si este resultado no es una instancia de <see cref="Fracaso{T}"/>, el método devolverá <c>null</c>.
+		/// </para>
 		/// </summary>
-		/// <returns>Una vista parametrizada fallida del resultado, o null.</returns>
-		public abstract Fracaso<T> AsFracaso();
+		/// <returns>Una vista de fracaso del resultado, o <c>null</c>.</returns>
+		public virtual Fracaso<T> AsFracaso() => null;
 
 		/// <summary>
-		/// <para>Obtiene un resultado, instancia de <see cref="Exito{T}"/> o <see cref="Fracaso{T}"/>, en base a
-		/// un valor de tipo parametrizado, una condición parametrizada y un mensaje de error.</para>
+		/// <para>Obtiene un resultado, instancia de <see cref="Exito{T}"/> o de <see cref="Fracaso{T}"/>, en base a
+		/// un valor de un tipo parametrizado, una condición parametrizada y un mensaje de error.</para>
 		/// <para>Si la condición parametrizada es cierta, se devolverá un resultado parametrizado exitoso conteniendo
 		/// el valor de tipo parametrizado indicado, mientras que si es falsa se devolverá un resultado parametrizado
-		/// fallido, acompañado por el mensaje de error indicado.</para>
+		/// de fracaso, acompañado por el mensaje de error indicado. Y si el mensaje de error indicado no es
+		/// significativo (nulo, vacío o blanco), se sustituirá por un mensaje genérico, como se indica en
+		/// <see cref="Fracaso{T}.Fracaso(string)"/>.</para>
 		/// </summary>
-		/// <param name="valor">Valor de tipo parametrizado devuelto por la operación que devuelve el resultado,
-		/// en caso de ser exitosa.</param>
+		/// <param name="valor">Valor devuleto por una operación en el resultado construido, en caso de éxito.</param>
 		/// <param name="exito">Condición de tipo parametrizado que indica en qué caso el resultado es exitoso.</param>
-		/// <param name="error">Mensaje de error en caso de que el resultado obtenido sea fallido.</param>
-		/// <returns>Un resultado parametrizado, exitoso o fallido en base a la condición indicada.</returns>
-		public static Resultado<T> Get(T valor, Predicate<T> exito, string error)
+		/// <param name="mensaje">Mensaje asociado en caso de que el resultado obtenido sea de fracaso.</param>
+		/// <returns>Un resultado parametrizado, de éxito o fracaso, en función de la condición indicada.</returns>
+		public static Resultado<T> Get(T valor, Predicate<T> exito, string mensaje)
 		{
-			if (exito == null) return new Fracaso<T>(error);
+			if (exito == null) return new Fracaso<T>(mensaje);
 			if (exito(valor)) return new Exito<T>(valor);
-			else return new Fracaso<T>(error);
+			else return new Fracaso<T>(mensaje);
 		}
 	}
 }
